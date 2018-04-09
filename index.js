561d7f8532acfe5c11dd50da1af15434b25bc019
ThomasGiulianelli <giuliat3@tcnj.edu>
2018-04-09 17:08:40 -0400

    updated csv format

--- index.js
+++ index.js
@@ -8,21 +8,33 @@
 const { HLTV } = require('hltv')
 var fs = require("fs");
 
+console.log('Fetching match results...');
 HLTV.getResults({pages: 1}).then((res) => {
 	writeCSV(res);
 })
 
 /* Fill csv file with relevant data */
 writeCSV = function(stats){
+	console.log('Creating csv at output/CSGO_Matches.csv');
 	var csvWriter = fs.createWriteStream('output/CSGO_Matches.csv', {flags: 'w'})
+	csvWriter.write("Team,Team Score,Opponent,Opponent Score"); //write csv header
+	
+	/* Loop through array of match results */
 	for (var i = 0; i < stats.length; i++) {
 		var matchData = stats[i];
+		
+		/* Only include matches where both teams are ranked in the top 20 in the world */
 		if (matchData.stars >= 2) {
-			//console.log(matchData.team1.name + " " + matchData.result + " " + matchData.team2.name);
-			csvWriter.write(matchData.team1.name + "," + matchData.result + "," + matchData.team2.name + "\n");
+			/* Extract score for each team */			
+			var result = matchData.result;
+			var resultArray = result.split(" - ");
+			var score1 = resultArray[0];
+			var score2 = resultArray[1];
+			
+			/* Append new record to file */
+			csvWriter.write("\n" + matchData.team1.name + "," + score1 + "," + matchData.team2.name + "," + score2);
 		}
 	}
 	csvWriter.end(); //close file
+	console.log('Done!');
 }
-
-console.log('program end');
