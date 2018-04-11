9f4d26d71805ed398eab94bc6e3dd1462147c03a
ThomasGiulianelli <giuliat3@tcnj.edu>
2018-04-11 01:06:42 -0400

    expanded to create a second csv for binary classification

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ HLTV.getResults({pages: 150}).then((res) => {
 writeCSV = function(stats){
 	console.log('Creating csv at output/CSGO_Matches.csv');
 	var csvWriter = fs.createWriteStream('output/CSGO_Matches.csv', {flags: 'w'})
+	var csvWriter2 = fs.createWriteStream('output/CSGO_Matches_Binary.csv', {flags: 'w'}) //for binary classification
 	csvWriter.write("Team,Team Score,Opponent,Opponent Score"); //write csv header
+	csvWriter2.write("Team,Team Score,Opponent,Opponent Score,Map,Result"); //write csv header
 	
 	/* Loop through array of match results */
 	for (var i = 0; i < stats.length; i++) {
@@ -26,18 +28,31 @@ writeCSV = function(stats){
 		/* Only include matches where both teams are ranked in the top 20 in the world */
 		if (matchData.stars >= 2) {
 			/* Extract score for each team */			
-			var result = matchData.result;
-			var resultArray = result.split(" - ");
-			var score1 = resultArray[0];
-			var score2 = resultArray[1];
+			var scoreResult = matchData.result;
+			var scoreResultArray = scoreResult.split(" - ");
+			var score1 = scoreResultArray[0]; //team score
+			var score2 = scoreResultArray[1]; //opponent score
+			
+			/* Convert scores to ints for comparison */
+			score1Int = parseInt(score1);
+			score2Int = parseInt(score2);
 			
 			/* Exclude Best of 3 and Best of 5 scores. 16 is the minimum required score to win a match */
-			if ((parseInt(score1) + parseInt(score2)) >= 16) {
+			if (score1Int + score2Int >= 16) {
+				/* determine if the team won or lost against their opponent */
+				if (score1Int > score2Int) {
+					var result = "win";
+				}
+				else var result = "loss";
+				
 				/* Append new record to file */
 				csvWriter.write("\n" + matchData.team1.name + "," + score1 + "," + matchData.team2.name + "," + score2);
+				csvWriter2.write("\n" + matchData.team1.name + "," + score1 + "," + matchData.team2.name + "," + score2 + "," + 
+				matchData.map + "," + result);
 			}
 		}
 	}
 	csvWriter.end(); //close file
+	csvWriter2.end(); //close file
 	console.log('Done!');
 }
